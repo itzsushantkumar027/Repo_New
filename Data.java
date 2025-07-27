5bf282d3f56b4978d4ab44b6d9f35b56fb7a0032
itzsushantkumar027 <itzsushantkumar@gmail.com>
2025-07-27 23:04:59 +0530

    Data.java

--- Data.java
+++ Data.java
@@ -1,16 +1,29 @@
-interface Animal {
-    void makeSound();
-}
-
-class Dog implements Animal {
-    public void makeSound() {
-        System.out.println("Bark");
-    }
-}
-
-public class Main {
+public class DataTypesDemo {
     public static void main(String[] args) {
-        Animal myDog = new Dog();
-        myDog.makeSound(); 
+        // Integer types
+        byte b = 100;                // 1 byte
+        short s = 10000;             // 2 bytes
+        int i = 100000;              // 4 bytes
+        long l = 10000000000L;       // 8 bytes
+
+        // Floating-point types
+        float f = 5.75f;             // 4 bytes
+        double d = 19.99;            // 8 bytes
+
+        // Character type
+        char c = 'A';                // 2 bytes (Unicode)
+
+        // Boolean type
+        boolean isJavaFun = true;
+
+        // Display the values
+        System.out.println("byte: " + b);
+        System.out.println("short: " + s);
+        System.out.println("int: " + i);
+        System.out.println("long: " + l);
+        System.out.println("float: " + f);
+        System.out.println("double: " + d);
+        System.out.println("char: " + c);
+        System.out.println("boolean: " + isJavaFun);
     }
 }
